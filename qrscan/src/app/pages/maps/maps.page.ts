115dd9be4291619842f4cbf5c3b1d3b3f7000ebe
zankeii <42954203+zankeii@users.noreply.github.com>
2019-07-02 13:11:00 -0400

    showing maps with received coordenates

--- qrscan/src/app/pages/maps/maps.page.ts
+++ qrscan/src/app/pages/maps/maps.page.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+
+declare var mapboxgl: any;
 
 @Component({
     selector: 'app-maps',
     templateUrl: './maps.page.html',
     styleUrls: ['./maps.page.scss'],
 })
-export class MapsPage implements OnInit {
+export class MapsPage implements OnInit, AfterViewInit {
     lat: number;
     lng: number;
 
@@ -22,6 +24,14 @@ export class MapsPage implements OnInit {
         this.lng = Number(geo[1]);
 
         console.log(this.lat, this.lng);
+    }
+
+    ngAfterViewInit(): void {
+        mapboxgl.accessToken = 'pk.eyJ1IjoibWlndWVseXVzdGl6IiwiYSI6ImNqeGx6dXV2ZDBib2Qzb3BzcG9jOTJpaDkifQ.6lm_hePv4aOb1st0Uzk-fA';
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
     }
 
 }
