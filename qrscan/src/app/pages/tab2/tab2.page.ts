2e1c52e83af263d6f5b4884a640f941aa239bf4e
zankeii <42954203+zankeii@users.noreply.github.com>
2019-07-01 22:00:17 -0400

    showing records in tab2

--- qrscan/src/app/pages/tab2/tab2.page.ts
+++ qrscan/src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DataLocalService} from "../../services/data-local.service";
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public datalocalService: DataLocalService) {}
+
+  enviarCorreo() {
+    console.log('Enviando correo...');
+  }
 
 }
