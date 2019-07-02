806f5e4b567ada6f7fd3a7cb1ccc2d7e654a6a57
zankeii <42954203+zankeii@users.noreply.github.com>
2019-07-02 11:01:45 -0400

    ionic storage
    implementing ionic storage to save the record

--- qrscan/src/app/app.module.ts
+++ qrscan/src/app/app.module.ts
@@ -9,11 +9,12 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [IonicStorageModule.forRoot(), BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- qrscan/src/app/services/data-local.service.ts
+++ qrscan/src/app/services/data-local.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {RegisterModel} from "../models/register.model";
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,20 @@ export class DataLocalService {
 
   saved: RegisterModel[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage) {
+    this.cargarStorage();
+  }
 
-  saveRegister(format: string, text: string) {
+  async cargarStorage() {
+    this.saved = await this.storage.get('registros') || [];
+  }
+
+  async saveRegister(format: string, text: string) {
+    await this.cargarStorage();
     const newRegister = new RegisterModel(format,text);
     this.saved.unshift(newRegister);
     console.log(this.saved)
+
+    this.storage.set('registros', this.saved);
   }
 }
